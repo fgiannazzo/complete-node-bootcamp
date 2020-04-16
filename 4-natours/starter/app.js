366dde6aaef508d0f9cfcceb56da703e5dcc94a9
Felipe Giannazzo <felipe@v1now.com>
2020-04-16 19:26:50 -0400

    s8l91

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,14 +1,26 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
+const favicon = require('serve-favicon');
+const path = require('path');
+
+// Require Rouiter Files
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 // 1) Middlewares
 
 const app = express();
 //third party middleware
-app.use(morgan('dev'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 //middleware to receive body requests from POST api requests
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
 //custom middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -16,129 +28,6 @@ app.use((req, res, next) => {
 });
 
 // 2) Route Handlers
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-const getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
-};
-
-const getTour = (req, res) => {
-  console.log(req.params);
-
-  // since ID is a string inside the JSON request, we need to convert it to integer using the multiply by one trick
-  const id = req.params.id * 1;
-
-  // find property loops through elements and only stores elements on the new variable that matches the if statement
-  const tour = tours.find((el) => el.id === id);
-
-  // if there is no tour that matches ID, return error
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-};
-
-const createTour = (req, res) => {
-  //   console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here>',
-    },
-  });
-};
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
 
 // to make parameters optional, add question mark at the end of it like this: /:y?
 
@@ -151,23 +40,9 @@ const deleteUser = (req, res) => {
 // or, with a different and prettier syntax:
 
 // 3) Routes (using mounted routers through middleware declarations)
-const tourRouter = express.Router();
-const userRouter = express.Router();
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-tourRouter.route('/').get(getAllTours).post(createTour);
-
-tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
-
-userRouter.route('/').get(getAllUsers).post(createUser);
-
-userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
-
 // 4) Start Server
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}....`);
-});
+module.exports = app;
